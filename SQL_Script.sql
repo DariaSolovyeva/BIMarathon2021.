cae6712792bd33b49f2a89bcb9dfbaffac0c4d82
DariaSolovyeva <36121575+DariaSolovyeva@users.noreply.github.com>
2021-10-22 19:03:11 -0700

    Update SQL_Script.sql

--- SQL_Script.sql
+++ SQL_Script.sql
@@ -1,10 +1,10 @@
 - Add Duplicate row in fact_table
 insert into fact_table
-           values(2382686, '2017', '2017','11','11', '10.7', '11.3', 1, 1, 2,1);
+     values (2382686, '2017', '2017','11','11', '10.7', '11.3', 1, 1, 2,1);
 
 -- method 1: Finding duplicates in table: fact_table using Group BY and HAVING 
 select YearStart, YearEnd, DataValue, DataValueAlt, LowConfidenceLimit, HighConfidenceLimit, Location_id,
-	   Topic_id, DataValueType_id, StratificationCategory_id , count(*) as Duplicates
+       Topic_id, DataValueType_id, StratificationCategory_id , count(*) as Duplicates
 from fact_table
 group by 1,2,3,4,5,6,7,8,9,10
 having count(*)>1;
@@ -13,7 +13,7 @@ having count(*)>1;
 WITH CTE as
 (
 select*, ROW_NUMBER() OVER (partition by YearStart, YearEnd, DataValue, DataValueAlt, LowConfidenceLimit,
-										 HighConfidenceLimit, Location_id, Topic_id, DataValueType_id, 
+			                 HighConfidenceLimit, Location_id, Topic_id, DataValueType_id, 
                                          StratificationCategory_id order by DataValue) Duplicates
 from fact_table
 ) 
@@ -26,3 +26,5 @@ select * from (select*, ROW_NUMBER() OVER (partition by YearStart, YearEnd, Data
                                                         StratificationCategory_id order by DataValue) as Duplicates
 from fact_table) as temp_table
 where Duplicates>1;
+
+
